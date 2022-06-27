b9abb3b26011ff9ed06e0e1e0965b95c32638f2f
okbuddyretaf <108250044+okbuddyretaf@users.noreply.github.com>
2022-06-26 21:42:33 -0700

    Update tecly.php

--- tecly.php
+++ tecly.php
@@ -1,3 +1,5 @@
+<?php
+
 // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
   $email = htmlspecialchars($_POST['email']);
